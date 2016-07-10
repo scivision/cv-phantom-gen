d10bda6db8d2e83ef71327d8b010a948864fa1ae
michael <nobody@noreply.github.com>
2016-07-09 22:12:54 -0400

    defaults and prereqs

--- AuroraPhantom.py
+++ AuroraPhantom.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     p.add_argument('-o','--format',help='write format (avi, png, pnm) for the output',default=None)
     p.add_argument('-t','--texture',help='select texture (vertsize,...)',default='vertsine')
     p.add_argument('-m','--motion',help='how the phantom moves (vertslide,horizslide,swirl,...)',default='horizslide')
-    p.add_argument('-b','--bits',help='bits per pixel of data',default=16,type=int)
+    p.add_argument('-b','--bits',help='bits per pixel of data',default=8,type=int)
     p.add_argument('-w','--width',help='feature width',type=float,default=30)
     p.add_argument('-n','--nframe',help='number of frames (time steps) to create',type=int,default=10)
     p.add_argument('--gausssigma',help='Gaussian std dev (only for gaussian phantoms)',type=float,default=35)
